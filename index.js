9bb64f61d751f59774ae3560965150620b8053f0
raygit2023 <145861556+raygit2023@users.noreply.github.com>
2023-10-21 23:09:34 +0800

    Update index.js with images and public as static pages

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 
 const app = express();
+// Function to serve all static files
+// inside public directory.
+app.use('/public',express.static('public'));
+app.use('/images', express.static('images'));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to my Express Application</h1><br> running in aws ecs service with ALB");
+  res.send("<h1>Welcome to my Express Application</h1><br> running in aws ecs service with ALB with public and images static page. <br> you can access images by /images/privacy.jpg ");
 });
 
 app.get("/info", (req, res) => {
   res.send("my information page with ALB in ECS EC2 ");
 });
-
+app.get("/about", (req, res) => {
+  res.send("About page with ALB in ECS EC2 ");
+});
 app.listen(3000, () => {
   console.log("listening");
 });
