a42eba1409ada063f0799abf1c61152ec7ba42ba
raygit2023 <145861556+raygit2023@users.noreply.github.com>
2023-10-21 17:11:22 +0800

    Update index.js with chnage message content with ALB

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to my Express Application</h1><br> running in aws ecs service");
+  res.send("<h1>Welcome to my Express Application</h1><br> running in aws ecs service with ALB");
 });
 
 app.get("/info", (req, res) => {
-  res.send("my information page ");
+  res.send("my information page with ALB in ECS EC2 ");
 });
 
 app.listen(3000, () => {
